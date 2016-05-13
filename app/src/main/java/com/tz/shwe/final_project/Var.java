88405c2283e8c77b0c276afadfed0e6cbf463503
Shwe Aye <shweaye810@gmail.com>
2016-05-12 20:22:54 -0700

    add equal function

--- app/src/main/java/com/tz/shwe/final_project/Var.java
+++ app/src/main/java/com/tz/shwe/final_project/Var.java
@@ -5,9 +5,18 @@ package com.tz.shwe.final_project;
  */
 public class Var extends Ex {
     String name;
+
     Var(String s) { name = s; }
+
     String to_string() {
         return name;
+    }
+    boolean equalsIgnoreCase(Var t) {
+        return this.name.equalsIgnoreCase(t.name);
+    }
+
+    boolean equals(Var t) {
+        return this.name.equals(t.name);
     }
 
 }
